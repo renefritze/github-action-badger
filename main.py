1e6282639b6135f04662e8899c3c5779060e3265
René Fritze <rene.fritze@wwu.de>
2020-07-24 17:21:40 +0200

    try to load context

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import os
+import pprint
+
 import requests  # noqa We are just importing this to prove the dependency installed correctly
 from github3 import login
 
@@ -10,7 +12,11 @@ def main():
     client = login(token=token)
     # my_input = os.environ["INPUT_MYINPUT"]
 
-    my_output = f"Hello! I am {client}"
+    event_data_file = os.environ["GITHUB_EVENT_PATH"]
+    import json
+    context = json.load(open(event_data_file))
+
+    my_output = f"Hello! I am {pprint.pformat(context.keys())}"
 
     print(f"::set-output name=myOutput::{my_output}")
 
